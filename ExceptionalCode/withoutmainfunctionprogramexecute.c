770a5dd44db53f62d592126114169490c6f4e148
ROHIT MALIK <30663345+rohitmalik10@users.noreply.github.com>
2019-09-29 22:17:57 +0530

    Add files via upload

---
+++ ExceptionalCode/withoutmainfunctionprogramexecute.c
@@ -0,0 +1,10 @@
+//main() function every program reason 1) entry point of every C/C++ program
+//2) all predefined and user defined functions are called directly or indirectly through main
+//Using macro defines main
+#include<stdio.h>
+#define fun main
+int fun(void)
+{
+	printf("Ramboo123");
+	return 0;
+}
