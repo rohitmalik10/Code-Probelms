76668ed861c73171c31aadd3f8c8a16cd9f99155
ROHIT MALIK <30663345+rohitmalik10@users.noreply.github.com>
2019-09-29 22:27:03 +0530

    Add files via upload

---
+++ ExceptionalCode/print100timeswithoutusingloopandrecursion.cpp
@@ -0,0 +1,9 @@
+#define a printf("1");  //print 1 only ones
+#define b a a a a a a a a a a // print a  10 times
+#define c b b b b b b b b b b // print b 10 times
+#include<stdio.h>
+int main()
+{
+	c;
+	return 0;
+}
