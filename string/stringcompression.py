33fa78d719a8249867d97cd877254807f5be3697
ROHIT MALIK <30663345+rohitmalik10@users.noreply.github.com>
2019-10-03 23:42:40 +0530

    Add files via upload

---
+++ string/stringcompression.py
@@ -0,0 +1,23 @@
+def string_compression(input):
+  
+  if len(input) == 0:
+    return ""
+  
+  counter = 1
+  output = ''
+  
+  for i in range(1,len(input)):
+  
+    if input[i] == input[i-1]:
+      counter += 1
+  
+    else:
+      output += input[i-1] + str(counter)
+      counter = 1
+  
+  output += input[-1] + str(counter)
+  
+  return output
+
+  
+print(string_compression("aaaacbbbbbccc"))
