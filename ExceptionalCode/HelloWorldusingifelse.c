c4b5e658cde06c7e0c53d43585b3b4c69ef523a1
ROHIT MALIK <30663345+rohitmalik10@users.noreply.github.com>
2019-10-04 23:25:44 +0530

    Add files via upload

---
+++ ExceptionalCode/HelloWorldusingifelse.c
@@ -0,0 +1,15 @@
+// if  "condition"
+      //     printf ("Hello");
+      // else
+      //     printf("World"); 
+// OUTPUT should beHelloworld
+//1)
+#include<stdio.h> 
+int main() 
+{ 
+    if(!printf("Hello")) 
+        printf("Hello"); 
+    else
+        printf("World"); 
+    getchar(); 
+}         
